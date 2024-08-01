7fabd20145e7a76174fc88dffa76427682e26142
Gihan Chamila <algihanchamila@gmail.com>
2024-08-01 21:56:23 +0530

    public layout, private layout developed

---
+++ src/component/layout/PrivateLayout.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Outlet } from "react-router-dom";
+import PrivateNavBar from "../PrivateNavBar.jsx"
+import { useAuth } from "../context/useAuth.jsx";
+
+const PrivateLayout = () => {
+    const auth = useAuth()
+
+    if(!auth){
+        <Navigate to="/login" />
+    }
+
+    return (
+        <>
+            <PrivateNavBar />
+            <Outlet />
+        </>
+    )
+}
+
+export default PrivateLayout
---
+++ src/component/layout/PublicLayout.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Outlet } from "react-router-dom";
+import PublicNavBar from "../PrivateNavBar.jsx"
+import { useAuth } from "../context/useAuth.jsx";
+
+const PublicLayout = () => {
+    const auth = useAuth()
+
+    if(auth){
+        <Navigate to="/" />
+    }
+
+    return (
+        <>
+            <PublicNavBar />
+            <Outlet />
+        </>
+    )
+}
+
+export default PublicLayout
