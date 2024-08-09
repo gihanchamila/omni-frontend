ea96538dd09728297a41299ebc78682a1a011999
Gihan Chamila <algihanchamila@gmail.com>
2024-08-09 19:56:23 +0530

    add backdrop blur

--- src/pages/category/CategoryList.jsx
+++ src/pages/category/CategoryList.jsx
@@ -224,7 +224,7 @@ const CategoryList = () => {
         <div
           id="popup-modal"
           tabIndex="-1"
-          className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+          className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full backdrop-brightness-50"
         >
         <div className="relative p-4 w-full max-w-md max-h-full">
           <div className="relative bg-white rounded-lg shadow">
