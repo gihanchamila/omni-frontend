7031afec0df62e1a27868d145cf80108e81e6f25
Gihan Chamila <algihanchamila@gmail.com>
2024-08-10 07:51:36 +0530

    getcategory using categoryId

--- src/pages/category/UpdateCategory.jsx
+++ src/pages/category/UpdateCategory.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from '../../utils/axiosInstance.js';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'sonner';
 
 const initialFormData = {title: "", description: ""}
@@ -8,6 +8,41 @@ const initialFormError = {title: ""}
 
 const UpdateCategory = () => {
 
+    const [formData, setFormData] = useState(initialFormData)
+    const [formError, setFormError] = useState(initialFormError)
+    const [loading, setLoading] = useState(false)
+
+    const navigate = useNavigate()
+    const params = useParams()
+    const categoryId = params.id
+
+    useEffect(() => {
+        if(categoryId){
+            const getCategory = async () => {
+                try{
+                    const response = await axios.get(`/category/${categoryId}`, formData)
+                    const data = response.data.data
+
+                    setFormData({
+                        title: data.category.title,
+                        description: data.category.description
+                    })
+                }catch(error){
+                    const response = error.response
+                    const data = response.data
+                    toast.error(data.message)
+                }
+            }
+            getCategory()
+        }
+
+    }, [categoryId])
+
+    const handleChange = (e) => {
+        setFormData((prev) => ({...prev, [e.target.name] : e.target.name}))
+    }
+
+    
     
 
   return (
