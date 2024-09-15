77a155e33d7a656906a1bf54aa9a7ba1eda764cc
Gihan Chamila <algihanchamila@gmail.com>
2024-09-15 10:42:23 +0530

    style changed

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -69,126 +69,182 @@ const Setting = () => {
       {/* Content Panel */}
       <div className="col-span-12 col-start-1 md:col-start-5 col-end-17 bg-gray-50 p-8 rounded-lg">
         {activeTab === "general" && (
-          <div>
-            <h2 className="title">General Settings</h2>
-            <div className="grid gap-y-4 md:grid-cols-1">
-              <h2 className="h6">Profile</h2>
-
+        <div>
+        <h2 className="title">General Settings</h2>
+        <div className="grid gap-y-4 md:grid-cols-1">
+          
+          {/* Profile Picture Section */}
+          <div className="bg-white p-6 rounded-lg">
+            <h2 className="h6">Profile Picture</h2>
+            <div className="flex flex-col md:flex-row items-center justify-between space-x-0 md:space-x-6">
+              
               {/* Profile Picture and Name */}
-              <div className="flex flex-col md:flex-row items-center justify-between space-x-0 md:space-x-6 p-6 border border-gray-200 rounded-lg">
-                <div className="relative flex flex-col items-center md:flex-row">
-                  <div>
-                    <img
-                      src={profilePic}
-                      alt="Profile"
-                      className="w-20 h-20 md:w-28 md:h-28 group rounded-full object-cover border-2 border-gray-300"
+              <div className="relative flex flex-col items-center md:flex-row bg-white p-4  rounded-lg">
+                <div>
+                  <img
+                    src={profilePic}
+                    alt="Profile"
+                    className="w-20 h-20 md:w-28 md:h-28 group rounded-full object-cover border-2 border-gray-300"
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-full opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer">
+                    <label htmlFor="profile-pic" className="cursor-pointer">
+                      <FiCamera className="text-white" />
+                    </label>
+                    <input
+                      id="profile-pic"
+                      type="file"
+                      accept="image/*"
+                      className="hidden"
+                      onChange={handleUpdatePic}
                     />
-                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-full opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer">
-                      <label htmlFor="profile-pic" className="cursor-pointer">
-                        <FiCamera className="text-white" />
-                      </label>
-                      <input
-                        id="profile-pic"
-                        type="file"
-                        accept="image/*"
-                        className="hidden"
-                        onChange={handleUpdatePic}
-                      />
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-center md:pl-6 md:items-start">
-                    <h2 className="text-lg font-semibold">John Doe</h2>
-                    <p className="text-gray-600">john.doe@example.com</p>
                   </div>
                 </div>
-
-                {/* Buttons */}
-                <div className="mt-4 md:mt-0 space-x-4">
-                  <Button variant="error" onClick={handleRemovePic}>
-                    Remove
-                  </Button>
-                  <Button variant="info">
-                    <label htmlFor="profile-pic">Update</label>
-                  </Button>
+                <div className="flex flex-col items-center md:pl-6 md:items-start">
+                  <h2 className="text-lg font-semibold">John Doe</h2>
+                  <p className="text-gray-600">john.doe@example.com</p>
                 </div>
               </div>
-
-              {/* Input Fields for Name and Email */}
-              <div>
-                <h2 className="subTitle">User Information</h2>
-                <div className="grid grid-cols-1 md:grid-cols-8 gap-x-4 gap-y-4">
-                  <div className="col-span-full md:col-start-1 md:col-end-5 space-y-4">
-                    {[
-                      { id: "name", label: "Full Name", type: "text", placeholder: "John Doe" },
-                      { id: "birthday", label: "Birth Day", type: "date" }
-                    ].map(({ id, label, type, placeholder }) => (
-                      <div className="flex flex-col space-y-1" key={id}>
-                        <label htmlFor={id} className="label">{label}</label>
-                        <input
-                          id={id}
-                          name={id}
-                          type={type}
-                          placeholder={placeholder}
-                          className="input-box"
-                        />
-                      </div>
-                    ))}
-                  </div>
-                  <div className="col-span-full md:col-start-5 md:col-end-9 space-y-4">
-                    {[
-                      { id: "email", label: "Email Address", type: "email", placeholder: "someone@gmail.com" },
-                      { id: "interests", label: "Interests", type: "text", placeholder: "e.g. Reading, Coding" }
-                    ].map(({ id, label, type, placeholder }) => (
-                      <div className="flex flex-col space-y-1" key={id}>
-                        <label htmlFor={id} className="label">{label}</label>
-                        <input
-                          id={id}
-                          name={id}
-                          type={type}
-                          placeholder={placeholder}
-                          className="input-box"
-                        />
-                      </div>
-                    ))}
-                  </div>
-
-                  {/* Save Changes Button */}
-                  <div className="col-span-full md:col-start-7 md:col-span-2 flex justify-center md:justify-end">
-                    <Button variant="info">Save Changes</Button>
-                  </div>
-                </div>
+              
+              {/* Buttons */}
+              <div className="mt-4 md:mt-0 space-x-4">
+                <Button variant="error" onClick={handleRemovePic}>
+                  Remove
+                </Button>
+                <Button variant="info">
+                  <label htmlFor="profile-pic">Update</label>
+                </Button>
               </div>
             </div>
           </div>
-        )}
+          
+          {/* User Information Section */}
+          <div className="bg-white p-6 rounded-lg">
+            <h2 className="subTitle">User Information</h2>
+            <div className="grid grid-cols-1 md:grid-cols-8 gap-x-4 gap-y-4">
+              <div className="col-span-full md:col-start-1 md:col-end-5 space-y-4">
+                {[ 
+                  { id: "name", label: "Full Name", type: "text", placeholder: "John Doe" },
+                  { id: "birthday", label: "Birth Day", type: "date" }
+                ].map(({ id, label, type, placeholder }) => (
+                  <div className="flex flex-col space-y-1" key={id}>
+                    <label htmlFor={id} className="label">{label}</label>
+                    <input
+                      id={id}
+                      name={id}
+                      type={type}
+                      placeholder={placeholder}
+                      className="input-box"
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className="col-span-full md:col-start-5 md:col-end-9 space-y-4">
+                {[ 
+                  { id: "email", label: "Email Address", type: "email", placeholder: "someone@gmail.com" },
+                  { id: "interests", label: "Interests", type: "text", placeholder: "e.g. Reading, Coding" }
+                ].map(({ id, label, type, placeholder }) => (
+                  <div className="flex flex-col space-y-1" key={id}>
+                    <label htmlFor={id} className="label">{label}</label>
+                    <input
+                      id={id}
+                      name={id}
+                      type={type}
+                      placeholder={placeholder}
+                      className="input-box"
+                    />
+                  </div>
+                ))}
+              </div>
 
+              {/* Save Changes Button */}
+              <div className="col-span-full md:col-start-7 md:col-span-2 flex justify-center md:justify-end">
+                <Button variant="info">Save Changes</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        )}
         {activeTab === "security" && (
           <div>
             <h2 className="text-2xl font-bold mb-6">Security Settings</h2>
-            <div className="space-y-6">
-              {/* Security settings fields */}
-              <div className="flex flex-col space-y-2">
-                <label htmlFor="password" className="text-gray-700 font-medium">Change Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  className="input-box px-4 py-2 border rounded-lg focus:outline-none"
-                  placeholder="Enter new password"
-                  required
-                />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {/* Change Password Section */}
+              <div className="flex flex-col space-y-6 bg-white p-6 rounded-lg">
+                <h3 className="text-lg font-semibold mb-4">Change Password</h3>
+                <div className="flex flex-col space-y-4">
+                  <div className="flex flex-col space-y-2">
+                    <label htmlFor="current-password" className="text-gray-700 font-medium">Current Password</label>
+                    <input
+                      type="password"
+                      name="current-password"
+                      id="current-password"
+                      className="input-box px-4 py-2 border rounded-lg focus:outline-none"
+                      placeholder="Enter current password"
+                      required
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-2">
+                    <label htmlFor="new-password" className="text-gray-700 font-medium">New Password</label>
+                    <input
+                      type="password"
+                      name="new-password"
+                      id="new-password"
+                      className="input-box px-4 py-2 border rounded-lg focus:outline-none"
+                      placeholder="Enter new password"
+                      required
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-2">
+                    <label htmlFor="confirm-new-password" className="text-gray-700 font-medium">Confirm New Password</label>
+                    <input
+                      type="password"
+                      name="confirm-new-password"
+                      id="confirm-new-password"
+                      className="input-box px-4 py-2 border rounded-lg focus:outline-none"
+                      placeholder="Confirm new password"
+                      required
+                    />
+                  </div>
+                </div>
+                <Button variant="info">Change Password</Button>
               </div>
 
-              <div className="flex items-center">
-                <input
-                  type="checkbox"
-                  name="two-factor"
-                  id="two-factor"
-                  className="form-checkbox"
-                />
-                <label htmlFor="two-factor" className="ml-2 text-gray-700">
-                  Enable Two-Factor Authentication
-                </label>
+              {/* Additional Security Settings Section */}
+              <div className="flex flex-col space-y-6 bg-white p-6 rounded-lg ">
+                <h3 className="text-lg font-semibold mb-4">Additional Security Settings</h3>
+                <div className="flex items-center space-x-2">
+                  <input
+                    type="checkbox"
+                    name="two-factor"
+                    id="two-factor"
+                    className="form-checkbox"
+                  />
+                  <label htmlFor="two-factor" className="text-gray-700">
+                    Enable Two-Factor Authentication
+                  </label>
+                </div>
+                <div className="flex flex-col space-y-2">
+                  <label htmlFor="backup-email" className="text-gray-700 font-medium">Backup Email Address</label>
+                  <input
+                    type="email"
+                    name="backup-email"
+                    id="backup-email"
+                    className="input-box px-4 py-2 border rounded-lg focus:outline-none"
+                    placeholder="Enter backup email address"
+                  />
+                </div>
+                <div className="flex flex-col space-y-2">
+                  <label htmlFor="security-questions" className="text-gray-700 font-medium">Security Questions</label>
+                  <textarea
+                    name="security-questions"
+                    id="security-questions"
+                    className="input-box px-4 border rounded-lg pb-5 focus:outline-none"
+                    placeholder="Answer security questions"
+                    rows="2"
+                  />
+                </div>
+                <Button variant="info">Save Security Settings</Button>
               </div>
             </div>
           </div>
