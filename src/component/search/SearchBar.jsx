31059b8d18825c093f6d2b736985ed5d27bbe07f
Gihan Chamila <algihanchamila@gmail.com>
2024-11-02 14:16:08 +0530

    changed button color

--- src/component/search/SearchBar.jsx
+++ src/component/search/SearchBar.jsx
@@ -23,7 +23,7 @@ const SearchBar = ({ currentPage, sortField, sortOrder, onSearchResults, searchV
                     required 
                 />
             </div>
-            <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg ">
+            <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-500 rounded-lg ">
                 <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                 </svg>
