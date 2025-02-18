377fbe9a76e672c4bb166e31257666ac8300951e
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-02-18 21:28:41 +0530

    update base url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Create an axios instance
-const axiosInstance = axios.create({baseURL : "http://localhost:8000/api/v1"})
+const axiosInstance = axios.create({baseURL: "http://localhost:8000/api/v1"})
 
 // Add a request interceptor
 axiosInstance.interceptors.request.use((req) => {
