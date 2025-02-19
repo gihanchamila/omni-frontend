3bfb8d6749bf95655f416fe7801812d1dd5140eb
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-02-19 13:46:12 +0530

    add local host for design update

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 // Create an axios instance
 // For the vercel
-const axiosInstance = axios.create({baseURL: import.meta.env.VITE_API_URL })
+
+// const axiosInstance = axios.create({baseURL: import.meta.env.VITE_API_URL })
 
 // For the localhost
-// const axiosInstance = axios.create({baseURL: "http://localhost:8000/api/v1"})
+const axiosInstance = axios.create({baseURL: "http://localhost:8000/api/v1"})
 
 // Add a request interceptor
 axiosInstance.interceptors.request.use((req) => {
