a9911268c825b4762b7a114fe4bfd57d692fba09
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-03-08 18:42:54 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,7 +3,10 @@ import axios from "axios";
 // Create an axios instance
 // For the vercel
 
-const axiosInstance = axios.create({baseURL: import.meta.env.VITE_API_URL })
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL, 
+    withCredentials: true, // if your API requires cookies/auth tokens
+});
 
 // For the localhost
 // const axiosInstance = axios.create({baseURL: "http://localhost:8000/api/v1"})
