e4e5f5ea3957f72fd6adc6d924ed64457a69df9a
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-03-13 11:27:51 +0530

    code clean

--- src/App.jsx
+++ src/App.jsx
@@ -39,61 +39,60 @@ function App() {
   
   return (
     <>
-    <div className="bg-white dark:bg-slate-900 min-w-screen min-h-screen ">
-      <div className="relative container">
-        <Suspense fallback={<div className="text-center p-5">Loading...</div>}>
-          <Routes>
-            <Route element={<PrivateLayout />}>
-              <Route path="categories" element={<CategoryList />} />
-              <Route path="categories/new-category" element={<NewCategory />} />
-              <Route path="categories/update-category/:id" element={<UpdateCategory />} />
-              <Route path="users" element={<Users />} />
-              <Route path="/" element={<PostList />} />
-              <Route path="posts/:id" element={<SinglePost />} />
-              <Route path="posts/new-post" element={<NewPost />} />
-              <Route path="posts/update-post/:id" element={<UpdatePost/>} />
-              <Route path="/:id" element={<Profile />} />
-              <Route path="user-profile/:id" element={<UserProfile/>} />
-              <Route path="settings" element={<Setting />} />
-              <Route path="dashboard" element={<Home/>} />
-              <Route path="admin-list" element={<AdminList/>} />
-              <Route path="notifications" element={<NotificationList/>} />
-            </Route>
-            <Route element={<PublicLayout />}>
-              <Route path="signup" element={<Signup />} />
-              <Route path="login" element={<Login />} />
-              <Route path="our-story" element={<HomePage />} />
-              <Route path="forgot-password" element={<ForgotPassword />}/>
-            </Route>
-          </Routes>
-        </Suspense>
-        
-        {/* 
-          <Toaster  closeButton={<HiOutlineXMark />} description
-            toastOptions={{
-              unstyled: true,
-              classNames: {
-                error: 'toast error',
-                success: 'toast success',
-                warning: 'toast warning',
-                info: 'toast info',
-                title: 'toastTitle',
-                closeButton: 'closeButton',
-                description: 'pt-2'
-              },
-            }}
+      <div className="bg-white dark:bg-slate-900 min-w-screen min-h-screen ">
+        <div className="relative container">
+          <Suspense fallback={<div className="text-center p-5">Loading...</div>}>
+            <Routes>
+              <Route element={<PrivateLayout />}>
+                <Route path="categories" element={<CategoryList />} />
+                <Route path="categories/new-category" element={<NewCategory />} />
+                <Route path="categories/update-category/:id" element={<UpdateCategory />} />
+                <Route path="users" element={<Users />} />
+                <Route path="/" element={<PostList />} />
+                <Route path="posts/:id" element={<SinglePost />} />
+                <Route path="posts/new-post" element={<NewPost />} />
+                <Route path="posts/update-post/:id" element={<UpdatePost/>} />
+                <Route path="/:id" element={<Profile />} />
+                <Route path="user-profile/:id" element={<UserProfile/>} />
+                <Route path="settings" element={<Setting />} />
+                <Route path="dashboard" element={<Home/>} />
+                <Route path="admin-list" element={<AdminList/>} />
+                <Route path="notifications" element={<NotificationList/>} />
+              </Route>
+              <Route element={<PublicLayout />}>
+                <Route path="signup" element={<Signup />} />
+                <Route path="login" element={<Login />} />
+                <Route path="our-story" element={<HomePage />} />
+                <Route path="forgot-password" element={<ForgotPassword />}/>
+              </Route>
+            </Routes>
+          </Suspense>
+          
+          {/* 
+            <Toaster  closeButton={<HiOutlineXMark />} description
+              toastOptions={{
+                unstyled: true,
+                classNames: {
+                  error: 'toast error',
+                  success: 'toast success',
+                  warning: 'toast warning',
+                  info: 'toast info',
+                  title: 'toastTitle',
+                  closeButton: 'closeButton',
+                  description: 'pt-2'
+                },
+              }}
 
-            icons={{
-              success: <HiCheckCircle className="iconSize" />,
-              info: <HiInformationCircle className="iconSize" />,
-              error: <HiExclamationCircle className="iconSize" />,
-              warning: <HiExclamation className="iconSize" /> 
-            }}
-          /> 
-        */}
+              icons={{
+                success: <HiCheckCircle className="iconSize" />,
+                info: <HiInformationCircle className="iconSize" />,
+                error: <HiExclamationCircle className="iconSize" />,
+                warning: <HiExclamation className="iconSize" /> 
+              }}
+            /> 
+          */}
+        </div>
       </div>
-    </div>
-      
     </>
   )
 }
