64964817c06c17195fc0a032774accdd56241679
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-03-13 10:12:01 +0530

    add comment

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+// Create an axios instance
+// Set the base URL to the API server URL 
+// import.meta.env.VITE_API_URL is the URL of the API server in backend on railway "https://omni-backend-production.up.railway.app"
+// The URL is stored in the .env file in the root directory of the frontend project
+
 const axiosInstance = axios.create({baseURL: import.meta.env.VITE_API_URL })
 
 // Add a request interceptor
