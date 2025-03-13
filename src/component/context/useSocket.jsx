14a31a8d88a2240086cf16fc7d2fb710363caea4
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-03-13 10:30:06 +0530

    add comments

--- src/component/context/useSocket.jsx
+++ src/component/context/useSocket.jsx
@@ -11,6 +11,7 @@ export const SocketProvider = ({ children }) => {
 
     // import.meta.env.VITE_SOCKET_URL is the URL of the socket server in backend on railway "https://omni-backend-production.up.railway.app"
     // The URL is stored in the .env file in the root directory of the frontend project
+    // VITE is used only for frontend environment variables
 
      const socket = io(import.meta.env.VITE_SOCKET_URL , {
         transports: ["websocket", "polling"]
