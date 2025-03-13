d38b97fc2ec87091605fab95eb1a936a6d13e55b
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-03-13 10:07:25 +0530

    clean code

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,12 +1,6 @@
 import axios from "axios";
 
-// Create an axios instance
-// For the vercel
-
 const axiosInstance = axios.create({baseURL: import.meta.env.VITE_API_URL })
-
-// For the localhost
-// const axiosInstance = axios.create({baseURL: "http://localhost:8000/api/v1"})
 
 // Add a request interceptor
 axiosInstance.interceptors.request.use((req) => {
